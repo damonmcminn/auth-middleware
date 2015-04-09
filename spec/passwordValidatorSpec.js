12a7e45ccf47b04b47016ad3e4ad84ddb3979390
Damon McMinn <damonrmcminn@gmail.com>
2015-04-09 18:09:44 +0100

    Put tests back into single describe block

--- spec/passwordValidatorSpec.js
+++ spec/passwordValidatorSpec.js
@@ -1,11 +1,39 @@
-describe('Password', function() {
+var auth = require('auth-utilities');
+var hash = auth.password(1).hash;
+var passwordValidator = require('../lib/passwordValidator');
 
-  var auth = require('auth-utilities');
-  var hash = auth.password(1).hash;
-  var passwordValidator = require('../lib/passwordValidator');
+describe('Password', function(done) {
 
   var validPassword = (new Buffer('user:password')).toString('base64');
   var invalidPassword = (new Buffer('user:invalidpassword')).toString('base64');
+
+  var errorFindingUser = passwordValidator({
+    findUser: function(email) {
+      return new Promise(function(resolve, reject) {
+        reject(new Error('find user error'));
+      });
+    },
+    secret: 'secret',
+    parse: auth.parseHeader('basic'),
+    type: function(req) {
+      return req.headers.authorization;
+    },
+    rounds: 1
+  });
+
+  var cannotFindUser = passwordValidator({
+    findUser: function(email) {
+      return new Promise(function(resolve, reject) {
+        resolve(false);
+      });
+    },
+    secret: 'secret',
+    parse: auth.parseHeader('basic'),
+    type: function(req) {
+      return req.headers.authorization;
+    },
+    rounds: 1
+  });
 
   var canFindUser = passwordValidator({
     findUser: function(email) {
@@ -72,4 +100,33 @@ describe('Password', function() {
       });
     });
   });
+
+  it('should call next() with err from findUser', function(done) {
+    hash('password').then(function(hashed) {
+      var req = {
+        headers: {
+          authorization: `Basic ${validPassword}`
+        }
+      };
+      errorFindingUser(req, null, function(err) {
+        expect(err.message).toBe('find user error');
+        done();
+      });
+    });
+  });
+
+  it('should call next() with Error if no user found', function(done) {
+    hash('password').then(function(hashed) {
+      var req = {
+        headers: {
+          authorization: `Basic ${validPassword}`
+        }
+      };
+      cannotFindUser(req, null, function(fromNext) {
+        expect(fromNext.message).toBe('User not found');
+        done();
+      });
+    });
+  });
+
 });
